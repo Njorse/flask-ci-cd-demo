8c7cf8e65e467af9c85bad67239acf98f6586fd7
Niesl <foundnyj@gmail.com>
2026-02-25 13:57:39 -0500

    Corrigiendo texto para pasar el test

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "¡Hola, CI/CD con DevOps!"
-
+    return "Hola, CI/CD con DevOps!"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- test_app.py
+++ test_app.py
@@ -3,4 +3,4 @@ from app import app
 def test_home():
     client = app.test_client()
     response = client.get("/")
-    assert response.data == b"\xc2\xa1Hola, CI/CD con DevOps!"
+    assert response.data == b"Hola, CI/CD con DevOps!"
